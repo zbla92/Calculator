0e85f6148440a16afa1bdc74b225bf22190dd042
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-10-21 16:43:44 +0200

    v 0.0.1
    Variabled Classes for EventListeners

--- script.js
+++ script.js
@@ -48,16 +48,35 @@ var calculatorController = (function(){
 //////////     UI Controller  / //////////////
 //////////////////////////////////////////////
 
-var UIController
-
-
-
-
-
-
-
-
-
+var UIController = (function(){
+    //Class to variables
+    var DOMvariables = {
+        percentageBtn: '.id__percentage',
+        squareRootBtn: '.id__squareRoot',
+        squareBtn:'.id__square',
+        underXbtn: '.id__underX',
+        btnCE: '.id__CE',
+        clearBtn: '.id__clearAll',
+        eraseBtn: '.id__erase',
+        devideBtn: '.id__devide',
+        multiplyBtn: '.id__times',
+        minusBtn: '.id__minus',
+        plusBtn: '.id__plus',
+        plusMinusBtn: '.id__plusMinus',
+        pointBtn: '.id__point',
+        btn9: '.id__9',
+        btn8: '.id__8',
+        btn7: '.id__7',
+        btn6: '.id__6',
+        btn5: '.id__5',
+        btn4: '.id__4',
+        btn3: '.id__3',
+        btn2: '.id__2',
+        btn1: '.id__1',
+        btn0: '.id__0',
+    }
+     
+})();
 
 
 
@@ -92,3 +111,29 @@ var UIController
 //////////////////////////////////////////////
 ////////// App Controller //////////////
 //////////////////////////////////////////////
+
+var controller = (function(){
+    
+    /////////////////////EVENT LISTENERS//////////////////
+    
+    document.querySelector('.id__equal').addEventListener('click', function(){
+        // Izracunaj
+    });
+    
+    document.querySelector(.id)
+    
+    
+    
+    
+})();
+
+
+
+
+
+
+
+
+
+
+//
